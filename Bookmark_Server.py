9bd799eb2096c7015a09ed1086af1c5a7bee6be6
Paul Foley <me@paulnicholasfoley.com>
2017-08-06 15:53:24 -0600

    Add Multiple Threading
    Added in functionality that allows the app to reference itself

--- Bookmark_Server.py
+++ Bookmark_Server.py
@@ -43,6 +43,8 @@
 
 import http.server
 import requests
+import threading
+from socketserver import ThreadingMixIn
 from urllib.parse import unquote, parse_qs
 import os
 
@@ -140,8 +142,11 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write("Couldn't fetch URI'{}'. Sorry!".format(longuri).encode())
 
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
